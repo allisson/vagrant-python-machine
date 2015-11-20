95ffce6932e7972f0c9da72b8b181a858f707b19
Allisson Azevedo <allisson@gmail.com>
2015-11-20 10:51:57 -0300

    Update libsass install script.

--- provisioning/roles/libsass/files/libsass-install.bash
+++ provisioning/roles/libsass/files/libsass-install.bash
@@ -1,14 +1,14 @@
 #!/bin/bash
-wget https://github.com/sass/sassc/archive/3.1.0.tar.gz
-tar xzf 3.1.0.tar.gz
-cd sassc-3.1.0
-wget https://github.com/sass/libsass/archive/3.1.0.tar.gz
-tar xzf 3.1.0.tar.gz
-cd libsass-3.1.0
+wget https://github.com/sass/sassc/archive/3.3.2.tar.gz
+tar xzf 3.3.2.tar.gz
+cd sassc-3.3.2
+wget https://github.com/sass/libsass/archive/3.3.2.tar.gz
+tar xzf 3.3.2.tar.gz
+cd libsass-3.3.2
 export SASS_LIBSASS_PATH="$(pwd)"
 cd ..
 make
 sudo cp bin/sassc /usr/local/bin
 cd ..
-rm -rf sassc-3.1.0
-rm 3.1.0.tar.gz
+rm -rf sassc-3.3.2
+rm 3.3.2.tar.gz
